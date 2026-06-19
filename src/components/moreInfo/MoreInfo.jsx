0de19f589377db30af8dac618924748d1290ab9d
Leticia Costa <leticiacmgomes@gmail.com>
2026-06-19 15:41:27 -0300

    adicionando key no component info

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -11,20 +11,24 @@ export const MoreInfo = () => {
 
   const infos = [
     {
+      id: 1,
       label: "current timezone",
-      info: time.timezone.replace("_", " "),
+      info: time?.timezone.replace("_", " "),
     },
     {
+      id: 2,
       label: "day of the year",
-      info: time.day_of_year,
+      info: time?.day_of_year,
     },
     {
+      id: 3,
       label: "day of the week",
-      info: time.day_of_week,
+      info: time?.day_of_week,
     },
     {
+      id: 4,
       label: "week number",
-      info: time.week_number,
+      info: time?.week_number,
     },
   ];
   return (
@@ -32,7 +36,7 @@ export const MoreInfo = () => {
       {time && (
         <>
           {infos.map((info) => (
-            <InfoContainer>
+            <InfoContainer key={info.id}>
               <InfoLabel>{info.label}</InfoLabel>
               <InfoText>{info.info}</InfoText>
             </InfoContainer>
